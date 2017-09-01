448735789444e0eabac60980a0c28ca47d823fbd
Daniel Rodriguez <daniel@foxylabs.gt>
2017-09-01 15:33:08 -0600

    [ADDED] agregado modulo consignacion por pagar

---
+++ app/plantilla/consignacionxCobrar/Cuerpo.php
@@ -0,0 +1,283 @@
+<div id="contenidoCrud">
+  
+    
+    <ul class="collapsible popout" data-collapsible="accordion">
+        <li>
+            <div class="collapsible-header"><i class="material-icons"><img class="imgSub" src="../app/img/flecha-hacia-abajo-signo-para-navegar.png" /></i>Consignacion</div>
+                <div class="collapsible-body">        
+                    <div class="col s12">
+                        <ul id="tabsn" class="tabsUsuarios centrartab blue darken-1 ">
+                            <div class="lipUsuario">
+                                <li class="centrarli"><a id="cobrar" href="#" class="amber accent-4 btn white-text tamatabsa  "><i class="material-icons left"><img class="iconotab" src="../app/img/cuentac.png" /></i>Cobrar</a></li>
+                                <li class="centrarli"><a id="pagar" href="#" class=" amber accent-4 btn white-text tamatabsa  "><i class="material-icons left"><img class="iconotab" src="../app/img/cuentap.png" /></i>Pagar</a></li>
+                                <li class="centrarli"><a id="consignacion" href="#" class="amber accent-4 btn white-text tamatabsa  "><i class="material-icons left"><img class="iconotab" src="../app/img/avatar.png" /></i>Consignacion Por Pagar</a></li>
+                                <li class="centrarli"><a id="consignacionxCobrar" href="#" class="yellow darken-4 btn white-text tamatabsa  "><i class="material-icons left"><img class="iconotab" src="../app/img/avatar.png" /></i>Consignacion Por Cobrar</a></li>
+                        <!-- <div class="indicator blue" style="z-index:1"></div>  -->
+                            </div>
+                        </ul>
+                     </div>
+                </div>
+        </li>
+    </ul>         
+
+
+
+    <!-- ********************************** tabla inicio ********************************** -->
+               <!-- <center>
+                  <li class="centrarli"><a id="imprimirT" onClick="printReporte(3);" class="amber accent-3 btn white-text  " ><i class="material-icons left"><img class="iconotab" src="../app/img/imprimir.png" /></i>Reporte de Proveedores</a></li>
+                  <li class="centrarli"><a id="imprimirT" onClick="printReporte(4);" class="amber accent-3 btn white-text  " ><i class="material-icons left"><img class="iconotab" src="../app/img/imprimir.png" /></i>Cuentas Vencidas</a></li>
+                </center>
+                <br>-->
+      <div style="display:none;">
+        <center>
+            <div class="radioFiltro">
+                <i class="material-icons prefix"><img class="iconologin radioBoton" src="../app/img/motocicleta.png" /></i>
+                <input class="radioColor" name="filtro" value="1" checked type="radio" id="motos" onChange=" mostrarCuentasP();" />
+                <label for="motos">Motos</label>
+            </div>
+
+            <div class="radioFiltro carroEspacio">
+                <i class="material-icons prefix"><img class="iconologin radioBoton" src="../app/img/coche.png" /></i>
+                <input class="radioColor" name="filtro" value="2" type="radio" id="carros" onChange=" mostrarCuentasP();" />
+                <label for="carros">Carros</label>
+            </div>
+        </center>
+    </div>
+
+                <div class="col s12 ">
+                    <div id="mensaje3"></div>
+                    <!-- reumen -->
+                    <div id="tablaMostrar">
+                    <?php 
+						include('../vista/cuentasPVista.php');
+						//mostrarCuentasP();
+				
+				
+						?>
+                    </div>
+                </div>
+   
+    <div class="centrartabla">
+
+
+      
+
+        
+
+
+        <!-- ********************************** modal ********************************** --> 
+
+        <!-- nuevo --> 
+
+        <div id="modal1" class="modal">
+           
+              
+                    	
+                  
+                        <div class="nav-wrapper grey darken-4">
+                            <div>
+                                <p class="encabezadotextomodal">Consignacion</p>
+
+                                <a id="modalcerrar" class=" modal-action modal-close waves-effect waves-light right  " ><i class="material-icons prefix"><img class="iconocerrarmodal" src="../app/img/desenfrenado.png"></i></a> 
+                            </div>	
+						</div>
+                      
+                      
+                      
+                      
+                       <div class="row">
+     
+        
+         
+      
+                      <div class="col s12 ">
+         
+         
+ <div id="mensajecv"></div>
+         
+                
+           			<div class="input-field col s5" hidden>
+                                  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/monto.png"/></i>
+								  <input  id="codigo" disabled type="text" class="validate">
+								  <label class="active" for="fecha" >Total Credito</label>
+								</div>
+                                <div class="input-field col s6">
+
+										 <i class="material-icons prefix"><img class="iconologin" src="../app/img/usuario.png" /> </i>
+										<input id="proveedorED" disabled type="text" onKeyUp="buscarCliente(this,event)" class="validate">
+										 <label for="icon_prefix" ><span class="etiquelogin">Proveedor </span></label>        
+							  </div>
+                             <div class="input-field col s6">
+                                  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/monto.png"/></i>
+								  <input  id="totalCreditoED" disabled type="number" class="validate">
+								  <label class="active" for="fecha" >Total Consignacion</label>
+								</div>
+                              <div class="input-field col s6">
+
+           <i class="material-icons prefix"><img class="iconologin" src="../app/img/plazo.png" /> </i>
+          <input id="fechaInicialED" disabled type="date" class="validate">
+           <label class="active" for="fecha" >Fecha Inicio</label>
+                 
+         </div>
+                              
+                               <div class="input-field col s6">
+                                  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/monto.png"/></i>
+								  <input  id="saldoED" disabled type="number" class="validate">
+								  <label class="active" for="fecha" >Saldo</label>
+								</div>
+                               
+                               
+                             
+                             
+                              
+                                  <div class="input-field col s6">
+
+										 <i class="material-icons prefix"><img class="iconologin" src="../app/img/plazo.png" /> </i>
+										<input id="fechaPagoED" type="date" class="validate" value="<?php echo date('Y-m-d')?>">
+										 <label class="active" for="fecha" >Fecha de Pago</label>       
+							  </div>
+                              
+                               <div class="input-field col s6">
+                                  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/monto.png"/></i>
+								  <input  id="MontoED" type="number" onChange="verificaImpresion();" onkeyup="verificaImpresion();" class="validate" autofocus>
+								  <label class="active" for="fecha" >Monto</label>
+								</div>
+                              
+                              
+                             
+                                <div class="input-field col s10">   
+								  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/descripcion.png"/></i>
+								  <input id="descripcionED" type="text" class="validate">
+								  <label for="icon_telephone" ><span class="etiquelogin">Descripcion</span></label>
+							  </div>
+
+                               
+                               
+                              
+                                                 
+          
+				<!-- reumen --> 
+		          
+				 <p id="saldoE" class=" col s3 right">Saldo</p>
+				 <div id="resumenPEdit" class="col s10"   >
+               
+				    Tabla(fecha de pago, Descripcion, Abono, Credito)
+                     
+					 </div>   
+          
+                          
+                          <?php
+	  if($_SESSION['SOFT_ACCESOAgrega'.'cuentas']=='1')
+				{
+	  ?>
+                            <a id="guardar" class="waves-effect waves-light btn blue lighten-1 modal-trigger" onClick="abonarConsignacion();" ><i class="material-icons left"><img class="iconoaddcrud" src="../app/img/guardar.png" /></i>Guardar</a>
+                       
+                       
+                            
+                        
+                        
+                    <?php
+				}?>
+                   
+           </div>	   
+             <!-- <a id="imprimePro11"  onClick="imprimirCuentaPagar('codigo','mensajecv');" class="waves-effect waves-light btn green lighten-1 modal-trigger botonG " ><i class="material-icons left"><img class="iconoaddcrud" src="../app/img/imprimir.png" /></i>Imprimir Detalle</a> 
+          -->
+       
+      </div>
+      
+ </div>
+
+
+                       
+<!-- nuevo fin -->
+
+
+
+<!-- Ver --> 
+
+  <div id="modal2" class="modal">
+           
+              
+                    	
+                  
+                        <div class="nav-wrapper grey darken-4">
+                            <div>
+                                <p class="encabezadotextomodal">Consignacion</p>
+
+                                <a id="modalcerrar" class=" modal-action modal-close waves-effect waves-light right  " ><i class="material-icons prefix"><img class="iconocerrarmodal" src="../app/img/desenfrenado.png"></i></a> 
+                            </div>	
+						</div>
+                      
+                      
+                       <div id="mensajeccV"></div>
+                      
+                       <div class="row">
+     
+        
+         
+      
+                      <div class="col s12 ">
+         
+         
+ 
+         
+           
+                    <div class="input-field col s10">
+
+										 <i class="material-icons prefix"><img class="iconologin" src="../app/img/usuario.png" /> </i>
+										<input id="proveedorV" disabled type="text" class="validate">
+                                        <label for="icon_prefix" ><span class="etiquelogin">Proveedor </span></label>        
+							  </div>
+                             <div class="input-field col s10">
+                                  <i  class="material-icons prefix"><img class="iconologin" src="../app/img/calle.png"/></i>
+								  <input  id="direccionV" disabled type="text" class="validate">
+								  <label class="active" for="Direccion" >Direccion</label>
+                  
+								</div>
+                             
+                              
+                              
+                              <div class="input-field col s10">
+
+										 <i class="material-icons prefix"><img class="iconologin" src="../app/img/plazo.png" /> </i>
+										<input id="fechaCredito" disabled type="date" class="validate">
+										  <label class="active" for="fecha" >Fecha Inicio</label>          
+							  </div>
+                              
+                               
+                            
+                            
+                              
+                              
+                                  
+                                
+                               
+                               
+                               
+                              
+                                                 
+          
+				<!-- reumen --> 
+		          
+				 <p id="saldoV" class=" col s3 right">Saldo</p>
+				 <div id="resumenPVer" class="col s10"   >
+               
+				    Tabla(fecha de pago, Descripcion, Abono, Credito)
+                     
+					 </div>   
+          
+                          
+                         
+                      
+                    
+                   
+           </div>	   
+              
+       
+      </div>
+      
+ </div>
+<!-- Ver fin --->   
+
+<!-- ********************************** modal fin ********************************** -->  
+
---
+++ vista/consignacionxCobrarVista.php
@@ -0,0 +1,186 @@
+<?php
+
+
+
+function mostrarConsignacionxCobrar($dato)
+{
+
+    session_start();
+    
+    $busca="";
+    
+        if($_SESSION['SOFT_ROL']!='1' && $_SESSION['SOFT_ROL']!='0')
+        {
+            $busca="and c.idusuario='".$_SESSION['SOFT_USER_ID']."'";
+        }
+          //creacion de la tabla
+      ?>
+    
+      <table id='tabla' class='bordered centered highlight responsive-table centrarT'>
+          <thead>
+              <tr>
+                  <th>Fecha</th>
+                  <th>No. Comprobante</th>
+                  <th>Nit</th>
+                  <th>Cliente</th>
+                  <th>Total</th>
+                  <th>Tipo de Venta</th>
+                  <?php
+                  if($_SESSION['SOFT_ROL']=='1' || $_SESSION['SOFT_ROL']=='0')
+                      {
+                        ?>
+                  <th>Vendedor</th>
+                  <?php } ?>
+                    <th></th>
+    
+              </tr>
+          </thead>
+          <tbody>
+              <?php
+          $extra="";
+          $mysql = conexionMysql();
+           $sql = "SELECT c.fecha,c.nocomprobante,p.nit,p.nombre,c.total,(select tv.Descripcion from tipoventa tv where tv.idtipo=c.tipoventa),c.idventas,(select u.user from usuarios u where u.idusuarios=c.idusuario) FROM ventas c inner join cliente p on p.idcliente=c.idcliente inner join ventasdetalle cd on cd.idventa=c.idventas inner join productos pd on pd.idproductos=cd.idproductos where c.estado=5 and cd.estado=1 $busca group by c.idventas order by c.fecha desc";
+          $tabla="";
+          if($resultado = $mysql->query($sql))
+          {
+    
+              if(mysqli_num_rows($resultado)==0)
+              {
+                  $respuesta = "<div class='error'>No hay Compras BD vacia</div>";
+              }
+    
+              else
+              {
+                  $contaId=0;
+    
+                  while($fila = $resultado->fetch_row())
+                  {
+    
+                      $tabla .= "<tr>";
+    
+                      $tabla .="<td>"     .substr($fila["0"],0,10).    "</td>";
+                      $tabla .="<td>" .$fila["1"].      "</td>";
+                      $tabla .="<td>" .$fila["2"].      "</td>";
+                      $tabla .="<td>" .$fila["3"].      "</td>";
+                      $tabla .="<td>" .toMoney($fila["4"]).      "</td>";
+    
+                      $tabla .="<td>" .$fila["5"].      "</td>";
+                    if($_SESSION['SOFT_ROL']=='1' || $_SESSION['SOFT_ROL']=='0')
+                      {
+                    $tabla .="<td>" .$fila["7"].      "</td>";
+                    }
+                    $tabla .="<td class='anchoC'>";
+                      if($_SESSION['SOFT_ACCESOElimina'.'ventas']=='1')
+                      {
+                      $tabla .="<a class='waves-effect waves-light btn red lighten-1 modal-trigger botonesm ' onClick=\"anularVenta('".$fila["6"]."');\"><i class='material-icons left'><img class='iconoaddcrud' src='../app/img/boton-borrar.png' /></i></a>";
+                      }
+    
+    
+    
+                      $tabla .="<a class='waves-effect waves-light btn yellow dark-1 modal-trigger botonesm modalver'  onClick=\"buscarVenta('".$fila["6"]."');\"><i class='material-icons left'><img class='iconoeditcrud' src='../app/img/ojo.png' /></i></a>";
+                      //$tabla .="<a class='waves-effect waves-light btn green dark-1 modal-trigger botonesm'  onClick=\"imprimirFactura('".$fila["6"]."','mensaje3');\"><i class='material-icons left'><img class='iconoeditcrud' src='../app/img/imprimir.png' /></i></a></td>";
+                      $tabla .= "</tr>";
+                      $contaId++;
+                  }
+    
+                  $resultado->free();//librerar variable
+    
+    
+                  $respuesta = $tabla;
+              }
+          }
+          else
+          {
+              $respuesta = "<div class='error'>Error: no se ejecuto la consulta a BD</div>";
+    
+          }
+    
+          //cierro la conexion
+          $mysql->close();
+    
+          //debuelvo la variable resultado
+          return printf($respuesta);
+              ?>
+          </tbody>
+      </table>
+      <?php
+    
+}
+
+
+function mostrarMovimientosConsignacion($id)
+{
+
+    //creacion de la tabla
+?>
+
+<table id='tabla' class='bordered centered highlight responsive-table centrarT'>
+    <thead>
+        <tr>
+            <th>Fecha</th>
+            <th>Descripcion</th>
+            <th>Abono</th>
+            <!--<th>Credito</th>-->
+            
+            
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+    $mysql = conexionMysql();
+    $sql = "SELECT cc.fecha,cc.descripcion,cc.retirado,cc.consignado FROM consignacion cc  WHERE cc.idcompras=".$id['0'];
+    $tabla="";
+    if($resultado = $mysql->query($sql))
+    {
+
+        if(mysqli_num_rows($resultado)==0)
+        {
+            $respuesta = "<div class='error'>No hay movimientos BD vacia</div>";
+        }
+
+        else
+        {
+
+            while($fila = $resultado->fetch_row())
+            {
+
+                $tabla .= "<tr>";
+
+                $tabla .="<td>"     .$fila["0"].    "</td>";
+                $tabla .="<td>" .$fila["1"]."</td>";
+                $tabla .="<td>" .toMoney($fila["2"]).      "</td>";
+				//$tabla .="<td>" .toMoney($fila["3"]).      "</td>";
+               
+                $tabla .= "</tr>";
+            }
+
+            $resultado->free();//librerar variable
+
+
+            $respuesta = $tabla;
+        }
+    }
+    else
+    {
+        $respuesta = "<div class='error'>Error: no se ejecuto la consulta a BD</div>";
+
+    }
+
+    //cierro la conexion
+    $mysql->close();
+
+    //debuelvo la variable resultado
+    return printf($respuesta);
+        ?>
+    </tbody>
+</table>
+<?php
+
+}
+
+
+
+
+
+?>
